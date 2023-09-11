f1a198ea358e3c1da1cb6f53cd1f2aa128a0a663
lokendra <thelokendrasinghrathore@gmail.com>
2023-09-11 12:36:51 +0530

    add backend link

--- Scripts/profile.js
+++ Scripts/profile.js
@@ -1,5 +1,5 @@
 
-const BaseUrl_profile = `https://angry-cummerbund-newt.cyclic.app`
+const BaseUrl_profile = `https://confused-mite-bandanna.cyclic.app`
 
 
 const qrcodeuserdetails_qrpage11 = localStorage.getItem('qrcodeuserdetails') || null;
